edf4c2b25c8098ef9ecb166c59e497e84387e466
Gabriel Marques <gabriel.marquesdesouza28@gmail.com>
2019-11-29 14:07:59 -0300

    Add tutorial component

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import Home from './pages/Home'
+import Tutorial from './pages/Tutorial'
 import Game from './pages/Game'
 import Result from './pages/Result'
 
@@ -9,6 +10,7 @@ export default () =>
     <Switch name='http://localhost:3000'>
       <Route path="/" component={Home} exact />
       <Route path="/home" component={Home} />
+      <Route path="/tutorial" component={Tutorial} />
       <Route path="/game" component={Game} />
       <Route path="/result" component={Result} />
       <Route component={Home} />
