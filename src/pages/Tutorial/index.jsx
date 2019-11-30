d8dceadfe3cac7df718e450edde11a91d8e5565f
Gabriel Marques <gabriel.marquesdesouza28@gmail.com>
2019-11-30 09:08:21 -0300

    Add tutorial component style

--- src/pages/Tutorial/index.jsx
+++ src/pages/Tutorial/index.jsx
@@ -1,4 +1,29 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import Indicator from '../../components/Indicator'
+import Button from '../../components/Button'
 import './styles.css'
 
-export default () => <div className="tutorial">Tutorial</div>
+export default () => 
+  <div className="tutorial">
+    <div className="box">
+      <Indicator bar1="1" bar2="1" />
+      <main>
+        <article>
+        <p>Ao lado, estão os três indicadores do game. Quando você tomar suas decisões, eles poderão 
+           aumentar ou diminuir. Por isso, acompanhe-os para saber sua evolução no jogo.
+        </p>
+        <p>Você iniciará o game com os indicadores zerados. No decorrer do jogo, eles serão impactados 
+           por todas as suas decisões. Então, ﬁque atento!
+        </p>
+        <p>Antes de resolver cada situação, reﬂita como suas escolhas podem inﬂuenciar os indicadores.</p>
+        </article>
+        <Indicator bar1="1" bar2="1" />
+      </main>
+      <footer>
+        <Link to="/game">
+          <Button label="CONTINUAR" />
+        </Link>
+      </footer>
+    </div>
+  </div>
